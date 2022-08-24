66d2287bfe160ce03195c196dc86f051293fa287
Florian Spiegel <Florian.Spiegel@tum.de>
2022-08-24 10:50:58 +0200

    Initial change to sn for action_daemon

--- include/vda5050_connector/action_daemon.h
+++ include/vda5050_connector/action_daemon.h
@@ -14,8 +14,14 @@ using namespace std;
 class ActionDaemon: public Daemon
 {
 	private:
-	vda5050_msgs::InstantActions iActionMessage; /**< Empty documentation
-						       stub. */
+	vda5050_msgs::InstantActions iActionMessage; /**< Empty documentation stub. */
+	
+	// Declare all ROS subscriber and publisher topics for internal communication
+	ros::Subscriber orderActionSub; 	/** ordinary order actions from order_daemon to action_daemon*/
+	ros::Subscriber instantActionSub; 	/** instant actions from MC to action_daemon*/
+	ros::Publisher actionStatesPub; 	/** states of actions from action_daemon to state_daemon*/
+	ros::Publisher orderCancelPub; 		/** cancelled actions from action_daemon to order_daemon*/
+	ros::Publisher actionToAgvPub; 		/** queued actions from action_daemon to AGV*/
 	
 	public:
 	/**
@@ -24,7 +30,7 @@ class ActionDaemon: public Daemon
 	 * @param nh	Empty parameter description
 	 * @param daemonName	Empty parameter description
 	 */
-	ActionDaemon(ros::NodeHandle *nh, std::string daemonName);
+	ActionDaemon();
 
 	/**
 	 * Empty description.
@@ -49,6 +55,35 @@ class ActionDaemon: public Daemon
 	 * Empty description.
 	 */
 	void InstantActionsCallback(const vda5050_msgs::InstantActions::ConstPtr& msg);
+
+	/**
+	 * Empty description.
+	 */
+	std::string createPublishTopic();
+
+	/**
+	 * @brief callback for order topic from order_daemon
+	 * 
+	 * This callback is called when a new message arrives at the /orderAction topic.
+	 * Orders are queued into a FIFO queue.
+	 * The first element of that queue is sent to the AGV for exection.
+	 * 
+	 * @param msg message including the incoming order
+	 */
+	void OrderActionCallback(const std_msgs::String::ConstPtr& msg);
+
+	/**
+	 * @brief loop actions
+	 * 
+	 * get order actions
+	 * get instantAction topics
+	 * calculate queue
+	 * send queue to agv
+	 * send order cancellations to order_daemon
+	 * send action status to state_daemon
+	 * 
+	 */
+	void UpdateActions();
 
 };
 
--- src/action_daemon.cpp
+++ src/action_daemon.cpp
@@ -3,18 +3,28 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "vda5050_msgs/ActionStates.h"
 
 using namespace std;
 
 /*
  * Currently, the action daemon is only used for passing messages from mqtt topic to ros topic
  */
-ActionDaemon::ActionDaemon(ros::NodeHandle *nh, std::string daemonName) : Daemon(nh,daemonName)
+ActionDaemon::ActionDaemon() : Daemon(&(this->nh), "action_daemon")
 {
-	LinkPublishTopics(nh);
-	LinkSubscriptionTopics(nh);
+	LinkPublishTopics(&(this->nh));
+	LinkSubscriptionTopics(&(this->nh));
+
+	// Initialize internal topics
+	orderActionSub = nh.subscribe("orderAction", 1000, &ActionDaemon::OrderActionCallback, this);
+	instantActionSub = nh.subscribe("instantAction", 1000, &ActionDaemon::InstantActionsCallback, this);
+
+	actionStatesPub = nh.advertise<vda5050_msgs::ActionStates>("actionStates", 1000);
+	orderCancelPub = nh.advertise<std_msgs::String>("orderCancel", 1000);
+	actionToAgvPub = nh.advertise<std_msgs::String>("action_to_agv", 1000);
 }
- 
+
+
 void ActionDaemon::LinkPublishTopics(ros::NodeHandle *nh)
 {
 	std::map<std::string,std::string>topicList = GetTopicPublisherList();
@@ -50,4 +60,36 @@ void ActionDaemon::InstantActionsCallback(const vda5050_msgs::InstantActions::Co
 	iActionMessage.manufacturer=msg->manufacturer;
 	iActionMessage.serialNumber=msg->serialNumber;	
 	iActionMessage.instantActions=msg->instantActions;
+}
+
+void ActionDaemon::OrderActionCallback(const std_msgs::String::ConstPtr& msg)
+{
+  //queue new Action (FIFO)
+  ;
+}
+
+void ActionDaemon::UpdateActions()
+{
+	// get order actions
+	// get instantAction topics
+	// calculate queue
+	// send queue to agv
+	// send order cancellations to order_daemon
+	// send action status to state_daemon
+	;
+}
+
+int main(int argc, char **argv)
+{	
+	ros::init(argc, argv, "action_deamon");
+	ros::NodeHandle nh;
+
+	ActionDaemon actionDaemon;
+
+	while(ros::ok())
+	{
+		actionDaemon.UpdateActions();
+		ros::spinOnce();
+	}
+	return 0;
 }
