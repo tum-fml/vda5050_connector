cf9aa2316c39bd2cd35f183cd89adb7d2fa8a88d
Florian Spiegel <Florian.Spiegel@tum.de>
2022-09-29 08:30:30 +0200

    Add ActionElement, ROS callbacks, action queues
    * Add the class ActionElement to store information
    about actions (order and instant)
    * Add ROS topics to pause actions, stop driving and get driving state
    * Add ROS callbacks for order/instant actions,
    order trigger, AGV action state and driving state
    * Add a first draft for order queuing logic

--- include/vda5050_connector/action_daemon.h
+++ include/vda5050_connector/action_daemon.h
@@ -3,43 +3,101 @@
 #include <ros/ros.h>
 #include "daemon.h"
 #include "vda5050_msgs/InstantActions.h"
+#include "vda5050_msgs/ActionState.h"
+#include "std_msgs/Bool.h"
 #include <string>
+#include <list>
+#include <deque>
 
 using namespace std;
+
+
+/**
+ * @brief Class for storing information about a single action
+ * 
+ */
+class ActionElement
+{
+	private:
+	string actionId; /** Unique ID to identify the action*/
+	string blockingType; /** Blocking type of the action, Enum {NONE, SOFT, HARD}*/
+	string actionType; /** Identifies the function of the action*/
+	string actionDescription; /** Additional information on the action*/
+	vector<vda5050_msgs::ActionParameter> actionParameters; /** Array of actionParameter objects*/
+
+	public:
+	string state; /** blocking type of the action*/
+	
+	/**
+	 * @brief Construct a new Action Element object
+	 * 
+	 * @param incomingAction New incoming action
+	 * @param state State of the new action
+	 */
+	ActionElement(vda5050_msgs::Action* incomingAction, string state);
+
+	/**
+	 * @brief returns true if given ID is equal to ID of action element
+	 * 
+	 * @param actionId2 ID to compare
+	 * @return true if IDs are equal
+	 * @return false if IDs are not equal
+	 */
+	bool compareId(string actionId2);
+
+	/**
+	 * @brief Returns an action message composed of an ActionElement
+	 * 
+	 * @return vda5050_msgs::Action Action message to return
+	 */
+	vda5050_msgs::Action packAction();
+};
 
 /**
  * Daemon for processing of VDA 5050 action messages. Currently, the action
  * daemon is only used for passing messages from an MQTT topic to a ROS topic.
  */
-class ActionDaemon: public Daemon
+class ActionDaemon : public Daemon
 {
-	private:
-	vda5050_msgs::InstantActions iActionMessage; /**< Empty documentation stub. */
-	
+private:
+	std::list<ActionElement> activeActionList; /**List of actions to track the blocking type of all active actions*/
+
 	// Declare all ROS subscriber and publisher topics for internal communication
-	ros::Subscriber orderActionSub; 	/** ordinary order actions from order_daemon to action_daemon*/
-	ros::Publisher actionStatesPub; 	/** states of actions from action_daemon to state_daemon*/
-	ros::Publisher orderCancelPub; 		/** cancelled actions from action_daemon to order_daemon*/
-	
-	public:
+	ros::Subscriber orderActionSub; /** ordinary order actions from order_daemon to action_daemon*/
+	ros::Publisher actionStatesPub; /** states of actions from action_daemon to state_daemon*/
+	ros::Publisher orderCancelPub;	/** cancelled actions from action_daemon to order_daemon*/
+
+	bool isDriving; /** True, if the vehicle is driving*/
+
+protected:
+	deque<vda5050_msgs::Action> orderActionQueue; /** queue for keeping track of order actions*/
+	deque<vda5050_msgs::Action> instantActionQueue; /** queue for keeping track of instant actions*/
+	string currentActionState;
+
+public:
 	/**
 	 * Empty description.
-	 * 
+	 *
 	 * @param nh	Empty parameter description
 	 * @param daemonName	Empty parameter description
 	 */
 	ActionDaemon();
 
+	// Flags
+	bool order_action_flag;
+	bool instant_action_flag;
+	bool instant_action_waiting;
+
 	/**
 	 * Empty description.
-	 * 
+	 *
 	 * @param nh	Empty parameter description
 	 */
 	void LinkPublishTopics(ros::NodeHandle *nh);
 
 	/**
 	 * Empty description.
-	 * 
+	 *
 	 * @param nh	Empty parameter description
 	 */
 	void LinkSubscriptionTopics(ros::NodeHandle *nh);
@@ -52,38 +110,96 @@ class ActionDaemon: public Daemon
 	/**
 	 * Empty description.
 	 */
-	void InstantActionsCallback(const vda5050_msgs::InstantActions::ConstPtr& msg);
-
-	/**
-	 * Empty description.
-	 */
 	std::string createPublishTopic();
 
 	/**
-	 * @brief callback for order topic from order_daemon
-	 * 
+	 * @brief callback for order actions topic from order_daemon
+	 *
 	 * This callback is called when a new message arrives at the /orderAction topic.
-	 * Orders are queued into a FIFO queue.
+	 * Actions are queued into a FIFO queue.
 	 * The first element of that queue is sent to the AGV for exection.
-	 * 
-	 * @param msg message including the incoming order
+	 *
+	 * @param msg message including the incoming order action
 	 */
-	void OrderActionCallback(const vda5050_msgs::Action::ConstPtr& msg);
+	void OrderActionsCallback(const vda5050_msgs::Action::ConstPtr &msg);
+	
+	/**
+	 * @brief callback for instant Actions topic from master controll
+	 *
+	 * This callback is called when a new message arrives at the /instantActions topic.
+	 * Actions are queued into a FIFO queue.
+	 * The first element of that queue is sent to the AGV for exection.
+	 *
+	 * @param msg message including the incoming instant action
+	 */
+	void InstantActionsCallback(const vda5050_msgs::InstantActions::ConstPtr &msg);
+
+	/**
+	 * @brief callback for order trigger topic from order daemon
+	 * 
+	 * This callback is called when a new message arrives at the /orderTrigger topic.
+	 * A trigger contains the ID of an action and triggers
+	 * adding the corresponding action to the order action queue.
+	 * 
+	 * @param msg message including the action ID to trigger
+	 */
+	void OrderTriggerCallback(const std_msgs::String &msg);
+
+	/**
+	 * @brief callback for agvActionState topic from AGV
+	 *
+	 * This callback is called when a new message arrives at the /agvActionState topic.
+	 * The message contains the state of a single action.
+	 * The state is used to fill the actionStates sent to the state daemon
+	 * and to track the current state for action blocking evaluation.
+	 *
+	 * @param msg message including the state of an action
+	 */
+	void AgvActionStateCallback(const vda5050_msgs::ActionState::ConstPtr &msg);
+
+	/**
+	 * @brief callback for driving topic from AGV
+	 *
+	 * This callback is called when a new message arrives at the /driving topic.
+	 * The message contains the driving state of the AGV.
+	 * “true”: indicates that the AGV is driving and/or rotating.
+	 * Other movements of the AGV (e.g. lift movements) are not included here.
+	 * “false”: indicates that the AGV is neither driving nor rotating.
+	 *
+	 * @param msg message including the driving state of the AGV
+	 */
+	void DrivingCallback(const std_msgs::Bool::ConstPtr &msg);
 
 	/**
 	 * @brief loop actions
-	 * 
-	 * get order actions
-	 * get instantAction topics
-	 * calculate queue
-	 * send queue to agv
-	 * send order cancellations to order_daemon
-	 * send action status to state_daemon
-	 * 
+	 *
 	 */
 	void UpdateActions();
 
+	void AddActionToList(vda5050_msgs::Action *newAction, string newState);
+
+	bool RunningActionHardBlocking();
+
+	bool checkDriving();
+
+	std::list<ActionElement> GetActiveActions();
+
+	/**
+	 * @brief finds 
+	 * 
+	 * @return ActionElement Action with corresponding ID 
+	 */
+
+	/**
+	 * @brief Finds and returns the action with the requested ID
+	 * 
+	 * Finds the action in the activeActionList list which has the requested ID.
+	 * The corresponding action is returned.
+	 * 
+	 * @param actionId ID of the action to find within the active Actions
+	 * @return ActionElement 
+	 */
+	ActionElement* findAction(string actionId); 
 };
 
 #endif
-
--- src/action_daemon.cpp
+++ src/action_daemon.cpp
@@ -1,94 +1,305 @@
 #include "vda5050_connector/action_daemon.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
 #include <iostream>
 #include <vector>
 #include <string>
+#include <list>
 #include "vda5050_msgs/ActionStates.h"
+#include "vda5050_msgs/ActionState.h"
 #include "vda5050_msgs/Action.h"
 
 using namespace std;
 
-/*
- * Currently, the action daemon is only used for passing messages from mqtt topic to ros topic
- */
 ActionDaemon::ActionDaemon() : Daemon(&(this->nh), "action_daemon")
-{
+{ 
 	LinkPublishTopics(&(this->nh));
 	LinkSubscriptionTopics(&(this->nh));
 
 	// Initialize internal topics
-	orderActionSub = nh.subscribe("orderAction", 1000, &ActionDaemon::OrderActionCallback, this);
-	actionStatesPub = nh.advertise<vda5050_msgs::ActionStates>("actionStates", 1000);
+	orderActionSub = nh.subscribe("orderAction", 1000, &ActionDaemon::OrderActionsCallback, this);
+	actionStatesPub = nh.advertise<vda5050_msgs::ActionState>("actionStates", 1000);
 	orderCancelPub = nh.advertise<std_msgs::String>("orderCancel", 1000);
-}
 
+	// Initialize flags
+	order_action_flag = false;
+	instant_action_flag = false;
+	instant_action_waiting = false;
+}
 
 void ActionDaemon::LinkPublishTopics(ros::NodeHandle *nh)
 {
-	std::map<std::string,std::string>topicList = GetTopicPublisherList();
-	std::stringstream ss;
+	map<string, string> topicList = GetTopicPublisherList();
+	stringstream ss;
 
-	for(const auto& elem : topicList)
+	for (const auto &elem : topicList)
 	{
-		ss<< "/" << elem.second;
-		if (CheckTopic(elem.first,"actionToAgv"))
+		ss << "/" << elem.second;
+		if (CheckTopic(elem.first, "actionToAgv"))
 		{
-			messagePublisher[elem.second] = nh->advertise<vda5050_msgs::InstantActions>(ss.str(),1000);
+			messagePublisher[elem.second] = nh->advertise<vda5050_msgs::Action>(ss.str(), 1000);
 		}
-	}	
+		if (CheckTopic(elem.first, "pauseActions"))
+		{
+			messagePublisher[elem.second] = nh->advertise<std_msgs::String>(ss.str(), 1000);
+		}
+		if (CheckTopic(elem.first, "stopDriving"))
+		{
+			messagePublisher[elem.second] = nh->advertise<std_msgs::String>(ss.str(), 1000);
+		}
+	}
 }
 
 void ActionDaemon::LinkSubscriptionTopics(ros::NodeHandle *nh)
 {
-	std::map<std::string,std::string>topicList = GetTopicSubscriberList();
-	for(const auto& elem : topicList)
+	map<string, string> topicList = GetTopicSubscriberList();
+	for (const auto &elem : topicList)
 	{
-		if (CheckTopic(elem.first,"actionFromMc"))
-			subscribers[elem.first]=nh->subscribe(elem.second,1000,&ActionDaemon::InstantActionsCallback, this);
-		if (CheckTopic(elem.first,"instantAction"))
-			subscribers[elem.first]=nh->subscribe(elem.second,1000,&ActionDaemon::InstantActionsCallback, this);
-	}	
+		if (CheckTopic(elem.first, "instantAction"))
+			subscribers[elem.first] = nh->subscribe(elem.second, 1000, &ActionDaemon::InstantActionsCallback, this);
+		if (CheckTopic(elem.first, "agvActionState"))
+			subscribers[elem.first] = nh->subscribe(elem.second, 1000, &ActionDaemon::AgvActionStateCallback, this);
+		if (CheckTopic(elem.first, "driving"))
+			subscribers[elem.first] = nh->subscribe(elem.second, 1000, &ActionDaemon::DrivingCallback, this);
+	}
 }
 
 void ActionDaemon::PublishActions()
-{	
-	messagePublisher["/action"].publish(iActionMessage);	
+{
+	// messagePublisher["/action"].publish();
 }
 
-void ActionDaemon::InstantActionsCallback(const vda5050_msgs::InstantActions::ConstPtr& msg)
+void ActionDaemon::InstantActionsCallback(const vda5050_msgs::InstantActions::ConstPtr &msg)
 {
-	iActionMessage.headerId=msg->headerId;
-	iActionMessage.timestamp=msg->timestamp;
-	iActionMessage.version=msg->version;
-	iActionMessage.manufacturer=msg->manufacturer;
-	iActionMessage.serialNumber=msg->serialNumber;	
-	iActionMessage.instantActions=msg->instantActions;
+	for (auto &elem : msg->instantActions)
+	{
+		// Push action to queue and add action to active actions list
+		vda5050_msgs::Action currAction = elem;
+		instantActionQueue.push_back(elem);
+		string actionStatus = "WAITING";
+		ActionDaemon::AddActionToList(&currAction, actionStatus);
+		
+		// Create and publish action state msg
+		vda5050_msgs::ActionState state_msg;
+		state_msg.header = ActionDaemon::GetHeader();
+		state_msg.actionID = elem.actionId;
+		state_msg.actionType = elem.actionType;
+		state_msg.actionStatus = actionStatus;
+		state_msg.resultDescription = ""; /*Description necessary?*/
+		actionStatesPub.publish(state_msg);
+
+		// Set instant action flag to trigger action handling 
+		instant_action_flag = true;
+	}
 }
 
-void ActionDaemon::OrderActionCallback(const vda5050_msgs::Action::ConstPtr& msg)
+void ActionDaemon::OrderActionsCallback(const vda5050_msgs::Action::ConstPtr &msg)
 {
-  //queue new Action (FIFO)
-  ;
+	// Add action to active actions list
+	vda5050_msgs::Action currAction = *msg;
+	string actionStatus = "WAITING";
+	ActionDaemon::AddActionToList(&currAction, actionStatus);
+	
+	// Create and publish action state msg
+	vda5050_msgs::ActionState state_msg;
+	state_msg.header = ActionDaemon::GetHeader();
+	state_msg.actionID = msg->actionId;
+	state_msg.actionType = msg->actionType;
+	state_msg.actionStatus = actionStatus;
+	state_msg.resultDescription = ""; /*Description necessary?*/
+	actionStatesPub.publish(state_msg);
+	
+	// Set order action flag to trigger action handling 
+	order_action_flag = true;
+}
+
+void ActionDaemon::OrderTriggerCallback(const std_msgs::String &msg)
+{
+	ActionElement *activeAction = findAction(msg.data);
+
+	if(activeAction)
+	{
+		// Push action to queue
+		orderActionQueue.push_back(activeAction->packAction());
+	}
+}
+
+void ActionDaemon::AgvActionStateCallback(const vda5050_msgs::ActionState::ConstPtr &msg)
+{
+	// currentActionState = msg->actionStatus;
+	// for (auto &action_it : activeActionList)
+	// {
+	// 	if (action_it.compareId(msg->actionID))
+	// 	{
+	// 		action_it.state = msg->actionStatus;
+	// 	}
+	// }
+	// vda5050_msgs::ActionStates actionStateMsgs;
+	// actionStateMsgs.actionStates.push_back(*msg);
+	// ActionDaemon::messagePublisher["/actionStates"].publish(*msg);
+}
+
+void ActionDaemon::DrivingCallback(const std_msgs::Bool::ConstPtr &msg)
+{
+	isDriving = msg->data;
+}
+
+void ActionDaemon::AddActionToList(vda5050_msgs::Action* incomingAction, string state)
+{
+	ActionElement newAction(incomingAction, state);
+	activeActionList.push_back(newAction);
+}
+
+bool ActionDaemon::checkDriving()
+{
+	if (isDriving)
+	{
+		std_msgs::String stopMsg;
+		stopMsg.data = "STOP";
+		messagePublisher["/stopDriving"].publish(stopMsg);
+		return false;
+	}
+	return true;
+}
+
+list<ActionElement> ActionDaemon::GetActiveActions()
+{
+	list<ActionElement> activeActions;
+	for (auto const& action_it : activeActionList)
+	{
+		if (action_it.state == "ACTIVE")
+		{
+			activeActions.push_back(action_it);
+		}
+	}
+	return activeActions;
+}
+
+ActionElement* ActionDaemon::findAction(string actionId)
+{
+	for (auto &elem : activeActionList)
+	{
+		if (elem.compareId(actionId))
+			return &elem;
+	}
+	ROS_INFO_STREAM("Invalid Action triggered!");
+	return nullptr;
 }
 
 void ActionDaemon::UpdateActions()
 {
-	// get order actions
-	// get instantAction topics
-	// calculate queue
-	// send queue to agv
-	// send order cancellations to order_daemon
-	// send action status to state_daemon
-	;
+
+	// Instant action routine
+	if(instant_action_flag)
+	{
+		;
+	}
+
+	// Order action routine
+	else if (order_action_flag && !instant_action_waiting)
+	{
+		if (!orderActionQueue.empty())
+		{
+			vda5050_msgs::Action nextActionInQueue = orderActionQueue[0];
+			bool RunningActionHardBlocking = false;
+			if (!RunningActionHardBlocking)
+			{
+				list<ActionElement> ActiveActions = GetActiveActions();
+
+
+
+				for (auto const& action_it : ActiveActions)
+				{
+					if (action_it.state == "PAUSED")
+					{
+						std_msgs::String pause;
+						pause.data = "RESUME";
+						messagePublisher["/pauseAction"].publish(pause);
+					}
+					else
+					{
+						string currBlockType = ActionDaemon::orderActionQueue.front().blockingType;
+						if (currBlockType == "HARD")
+						{
+							// Check running action
+							checkDriving();
+						}
+						else if (currBlockType == "SOFT")
+						{
+							checkDriving();
+						}
+						else if (currBlockType == "NONE")
+						{
+
+						}
+					}
+				}
+			}
+
+
+			// if (currentActionState == "RUNNING" || currentActionState == "INITIALIZING")
+			// {
+			// 	if (runningActionBlockingType != "HARD")
+			// 	{
+			// 		if (nextActionInQueue.blockingType == "SOFT")
+			// 		{
+			// 			if (isDriving == true)
+			// 			{
+			// 				messagePublisher["/stopDriving"].publish("STOP");
+			// 			}
+			// 			messagePublisher["/actionToAgv"].publish(nextActionInQueue);
+			// 			actionQueue.pop_front();
+			// 		}
+			// 		else if (nextActionInQueue.blockingType == "NONE")
+			// 		{
+			// 			messagePublisher["/actionToAgv"].publish(nextActionInQueue);
+			// 			actionQueue.pop_front();
+			// 		}
+			// 	}
+			// }
+			// else
+			// {
+			// 	messagePublisher["/actionToAgv"].publish(nextActionInQueue);
+			// 	actionQueue.pop_front();
+			// }
+		}
+	}
+}
+
+ActionElement::ActionElement(vda5050_msgs::Action* newAction, string newState)
+{
+	actionId = newAction->actionId;
+	blockingType = newAction->blockingType;
+	actionType = newAction->actionType;
+	actionDescription = newAction->actionDescription;
+	actionParameters = newAction->actionParameters;
+	state = newState;
+}
+
+bool ActionElement::compareId(string actionId2)
+{
+	return actionId == actionId2;
+}
+
+vda5050_msgs::Action ActionElement::packAction()
+{
+	vda5050_msgs::Action msg;
+	msg.actionId = actionId;
+	msg.blockingType = blockingType;
+	msg.actionType = actionType;
+	msg.actionDescription = actionDescription;
+	msg.actionParameters = actionParameters;
+	
+	return msg;
 }
 
 int main(int argc, char **argv)
-{	
+{
 	ros::init(argc, argv, "action_deamon");
 
 	ActionDaemon actionDaemon;
 
-	while(ros::ok())
+	while (ros::ok())
 	{
 		actionDaemon.UpdateActions();
 		ros::spinOnce();
