21d2c99e4ddb0f690d3de829c98327444fc6d515
Florian Rothmeyer <florian.rothmeyer@tum.de>
2022-12-20 14:03:39 +0100

    add license headers to all code files

--- src/mock_ups/state_mockup.cpp
+++ src/mock_ups/state_mockup.cpp
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2022 Technical University of Munich, Chair of Materials Handling,
+ * Material Flow, Logistics – All Rights Reserved
+ * 
+ * You may use, distribute and modify this code under the terms of the 3-clause
+ * BSD license. You should have received a copy of that license with this file.
+ * If not, please write to {kontakt.fml@ed.tum.de}.
+ */
+
 #include "ros/ros.h"
 #include <iostream>
 #include "vda5050_msgs/State.h"
